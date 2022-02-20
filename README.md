a79053c4f101af9aa12e7f7a6b8deaf79fdcf6b8
Alfredo Clark <alfredoclarkcardenas@gmail.com>
2022-02-19 20:40:07 -0700

    Template done

--- README.md
+++ README.md
@@ -8,5 +8,3 @@
 
 
 #### May the force be with all of you.
-
-![Wise Yoda](https://www.google.com/url?sa=i&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Frock-paper-scissors&psig=AOvVaw1UidQqPOoqg-T3RqhNotTb&ust=1645322205967000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNi72f_UivYCFQAAAAAdAAAAABAD)
---
+++ src/script.js
@@ -0,0 +1,44 @@
+var userChoice;
+var cpuChoice;
+var choices = ['rock', 'paper', 'scissors'];
+var userScore = 0;
+var computerScore = 0;
+
+function round(){
+  var userChoice = prompt('What do you choose?').toLowerCase();
+  var cpuChoice = choices[(Math.random() * choices.length) | 0];
+  console.log("You chose: " + userChoice);
+  console.log('The PC chose: ' + cpuChoice);
+  if ((userChoice === 'paper' && cpuChoice ==='scissors') || (userChoice === 'rock' && cpuChoice === 'paper') || (userChoice === 'scissors' && cpuChoice === 'rock')){
+    console.log('Point for the PC!')
+    computerScore++;
+  }
+  else if ((userChoice === 'paper' && cpuChoice === 'rock') || (userChoice === 'rock' && cpuChoice=== 'scissors') || (userChoice === 'scissors' && cpuChoice === 'paper')){
+    console.log('Point for you!')
+    userScore++;
+  }
+  else{
+    console.log('No points!')
+  }
+}
+
+
+function game(){
+  for (let i = 0; i < 5; i++) {
+    round();
+    console.log('You have: ' + userScore + ' points.');
+    console.log('PC has: ' + computerScore + ' points.');
+  }
+  if (userScore === computerScore){
+    console.log('Is a tie!')
+  }
+  else if(userScore < computerScore){
+    console.log('You lost!')
+  }
+  else {
+    console.log('You won!')
+  }
+}
+
+game();
+
